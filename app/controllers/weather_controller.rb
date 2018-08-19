db10d926e9272efd36953e9990d4cb80dee40626
jin521 <jinlc220@126.com>
2018-08-19 20:46:28 +1000

    remove empty method

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,9 +1,5 @@
 class WeatherController < ApplicationController
 
-  def create
-
-  end
-
   def forecast
     @forecasts = WeatherService.new(params[:city]).get_forecast
     render json: @forecasts
